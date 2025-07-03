701802f4e86aa6471b99cbc0ce01b8f848bb61c8
Kauã Victor <164793271+kauavictorss@users.noreply.github.com>
2025-07-03 16:50:02 -0300

    Iniciando Projeto do Sistema Eleitoral

---
+++ src/main/java/sisepe.api/SisepeApplication.java
@@ -0,0 +1,10 @@
+package sisepe.api;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisepeApplication {
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(SisepeApplication.class, args);
+    }
+}
